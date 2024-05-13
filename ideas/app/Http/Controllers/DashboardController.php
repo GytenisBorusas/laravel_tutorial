d77c581d786511d76e0fec18edf67734f03b1188
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-05-13 14:43:07 -0600

    done #10

--- ideas/app/Http/Controllers/DashboardController.php
+++ ideas/app/Http/Controllers/DashboardController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Idea;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $idea = new Idea();
-        $idea->content = 'Hello youtube!!!';
-        $idea->likes = 0;
-        $idea->save();
-
         return view('dashboard', [
             'ideas' => Idea::orderBy('created_at', 'desc')->get(),
         ]);
