f7a5b204ccfee8b338237b021e09f38992f399ed
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-05-13 16:14:35 -0600

    Done - Route model binding #15

--- ideas/app/Http/Controllers/IdeaController.php
+++ ideas/app/Http/Controllers/IdeaController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
+
+    public function show(Idea $idea)
+    {
+        return view('ideas.show', compact('idea'));
+    }
+
     public function store()
     {
 
@@ -26,10 +32,10 @@ class IdeaController extends Controller
 
     }
 
-    public function destroy($id)
+    public function destroy(Idea $idea)
     {
-        $idea = Idea::where('id',$id)->firstOrFail();
         $idea->delete();
+
         return redirect()->route('dashboard')->with('success','Idea deleted successfully');
     }
 }
