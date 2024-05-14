1499f2a0e4f579f403082af72432179cab4f9a7f
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-05-14 08:09:59 -0600

    CRUD:update #16

--- ideas/app/Http/Controllers/IdeaController.php
+++ ideas/app/Http/Controllers/IdeaController.php
@@ -8,23 +8,18 @@ use Illuminate\Http\Request;
 class IdeaController extends Controller
 {
 
-    public function show(Idea $idea)
-    {
+    public function show(Idea $idea){
         return view('ideas.show', compact('idea'));
     }
 
-    public function store()
-    {
-
-        request()->validate(
-            [
-                'idea' => 'required|min:3|max:240',
+    public function store(){
+        request()->validate([
+                'content' => 'required|min:3|max:240',
             ]
         );
 
-        $idea = Idea::create(
-            [
-                'content' => request()->get('idea',''),
+        $idea = Idea::create([
+                'content' => request()->get('content',''),
             ]
         );
 
@@ -32,10 +27,26 @@ class IdeaController extends Controller
 
     }
 
-    public function destroy(Idea $idea)
-    {
+    public function destroy(Idea $idea){
         $idea->delete();
-
         return redirect()->route('dashboard')->with('success','Idea deleted successfully');
     }
+
+    public function edit(Idea $idea) {
+        $editing = true;
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea) {
+        request()->validate([
+                'content' => 'required|min:3|max:240',
+            ]
+        );
+
+        $idea->content = request()->get('content');
+        $idea->save();
+
+        return redirect()->route('ideas.show', $idea->id)->with('success','Idea updated successfully');
+    }
+
 }
