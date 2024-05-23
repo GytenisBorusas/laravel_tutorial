a8540cd6408e85686f5ff5c6165df094caa20f1a
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-05-23 14:59:51 -0600

    Comments(relationships) #18

--- ideas/routes/web.php
+++ ideas/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'] )->name('dashboard');
@@ -15,6 +16,8 @@ Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'] )->name('ideas.
 Route::put('/ideas/{idea}', [IdeaController::class, 'update'] )->name('ideas.update');
 
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'] )->name('ideas.destroy');
+
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'] )->name('ideas.comments.store');
 
 Route::get('/terms', function () {
     return view('terms');
