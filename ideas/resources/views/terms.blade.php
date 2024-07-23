782dfc0963536356f79c1c3fb9b2a34c4fa574fb
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-07-23 10:00:13 -0600

    active menu links |#30

--- ideas/resources/views/terms.blade.php
+++ ideas/resources/views/terms.blade.php
@@ -1,9 +1,19 @@
-
 @extends('layout.layout')
 
 @section('content')
-    <h1>Terms</h1>
-    <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim provident repellendus soluta rem consequuntur neque, vero ad adipisci voluptatum, eius odit aperiam non esse! Maiores nihil tenetur nam vero ex.
-    </div>
-@endsection
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <h1>Terms</h1>
+            <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim provident repellendus soluta rem consequuntur
+                neque, vero ad adipisci voluptatum, eius odit aperiam non esse! Maiores nihil tenetur nam vero ex.
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    @endsection
